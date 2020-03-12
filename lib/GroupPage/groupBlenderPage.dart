8d1522a63574011034cb99cd7e96485721e4af35
fastriver <hykwyuk0125@gmail.com>
2020-03-13 00:44:53 +0900

    add ripple to group

--- lib/GroupPage/groupBlenderPage.dart
+++ lib/GroupPage/groupBlenderPage.dart
@@ -10,31 +10,36 @@ class GroupBlenderPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color(0x80FFFFFF),
-        body: LayoutBuilder(
-            builder: (context, boxConstraint) {
-              deviceInfo = DeviceInfo.measure(boxConstraint.biggest);
-              return GroupInfoLayout(
-                heroTag: "BLENDER",
-                color: Colors.orange,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text("Blender班", style: BaseTextStyles.h1(deviceInfo),),
-                  ),
-                  Image.asset("image/groups/blender.png", fit: BoxFit.fitWidth,),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("3Dモデルや3DCGが作れるBlenderというソフトを使って製作しています。", style: BaseTextStyles.plain,),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Text("閉じる", style: BaseTextStyles.button,),
-                  )
-                ],
-              );
-            }
+        body: InkWell(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: LayoutBuilder(
+              builder: (context, boxConstraint) {
+                deviceInfo = DeviceInfo.measure(boxConstraint.biggest);
+                return GroupInfoLayout(
+                  heroTag: "BLENDER",
+                  color: Colors.orange,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text("Blender班", style: BaseTextStyles.h1(deviceInfo),),
+                    ),
+                    Image.asset("image/groups/blender.png", fit: BoxFit.fitWidth,),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("3Dモデルや3DCGが作れるBlenderというソフトを使って製作しています。", style: BaseTextStyles.plain,),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("閉じる", style: BaseTextStyles.button,),
+                    )
+                  ],
+                );
+              }
+          ),
         )
     );
   }
--- lib/GroupPage/groupDTMPage.dart
+++ lib/GroupPage/groupDTMPage.dart
@@ -10,31 +10,36 @@ class GroupDTMPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color(0x80FFFFFF),
-        body: LayoutBuilder(
-            builder: (context, boxConstraint) {
-              deviceInfo = DeviceInfo.measure(boxConstraint.biggest);
-              return GroupInfoLayout(
-                heroTag: "DTM",
-                color: Colors.purple,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text("DTM班", style: BaseTextStyles.h1(deviceInfo),),
-                  ),
-                  Image.asset("image/groups/dtm.png", fit: BoxFit.fitWidth,),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("DTM班では各種DAW(Studio One,Cubase,Logicなどの作曲ソフト)を使って曲を作り、秋のM3でCDを売っています!普段はDAWの使い方、音楽理論の講習会等行っています!!", style: BaseTextStyles.plain,),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Text("閉じる", style: BaseTextStyles.button,),
-                  )
-                ],
-              );
-            }
+        body: InkWell(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: LayoutBuilder(
+              builder: (context, boxConstraint) {
+                deviceInfo = DeviceInfo.measure(boxConstraint.biggest);
+                return GroupInfoLayout(
+                  heroTag: "DTM",
+                  color: Colors.purple,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text("DTM班", style: BaseTextStyles.h1(deviceInfo),),
+                    ),
+                    Image.asset("image/groups/dtm.png", fit: BoxFit.fitWidth,),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("DTM班では各種DAW(Studio One,Cubase,Logicなどの作曲ソフト)を使って曲を作り、秋のM3でCDを売っています!普段はDAWの使い方、音楽理論の講習会等行っています!!", style: BaseTextStyles.plain,),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("閉じる", style: BaseTextStyles.button,),
+                    )
+                  ],
+                );
+              }
+          ),
         )
     );
   }
